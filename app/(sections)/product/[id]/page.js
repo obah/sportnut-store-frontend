f78c159cf7ac6f21dea39876a37f2ef35593eba6
Obaloluwa Olusoji <olusojiobah@gmail.com>
2023-06-28 11:37:23 +0100

    fixed product page

--- app/(sections)/product/[id]/page.js
+++ app/(sections)/product/[id]/page.js
@@ -1,48 +1,15 @@
-"use client";
-
-import { CartLogo } from "@/components/icons";
-import ProductImages from "@/components/productImages";
-import { CartContext } from "@/context/cartContext";
+import ProductPage from "@/components/productPage";
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
-import { useContext } from "react";
 
 export default async function Page({ params }) {
   const productId = params.id;
   const productData = await getProduct(productId);
   const product = productData.product;
 
-  const { addItemToCart } = useContext(CartContext);
-
-  //   const addThisItemToCart = () => {
-  //     addItemToCart();
-  //   };
-
-  const box = "bg-white rounded-sm p-8";
-
   return (
     <div className="center">
-      <div className="product-wrapper mt-10">
-        <div className={box}>
-          <ProductImages images={product.images} />
-        </div>
-        <div className="">
-          <h1 className="font-bold text-2xl">{product.name}</h1>
-          <p>{product.description}</p>
-          <div className="flex items-center gap-4 mt-4">
-            <span className="text-xl">₦{product.price}</span>
-            <button
-              onClick={() => {
-                addItemToCart(product._id);
-              }}
-              className="primary-btn px-4 py-1 flex items-center gap-2"
-            >
-              <CartLogo />
-              Add to cart
-            </button>
-          </div>
-        </div>
-      </div>
+      <ProductPage product={product} />
     </div>
   );
 }
---
+++ components/productPage.js
@@ -0,0 +1,36 @@
+"use client";
+
+import ProductImages from "./productImages";
+import { CartLogo } from "./icons";
+import { useContext } from "react";
+import { CartContext } from "@/context/cartContext";
+
+export default function ProductPage({ product }) {
+  const { addItemToCart } = useContext(CartContext);
+
+  const box = "bg-white rounded-sm p-8";
+
+  return (
+    <div className="product-wrapper mt-10">
+      <div className={box}>
+        <ProductImages images={product.images} />
+      </div>
+      <div className="">
+        <h1 className="font-bold text-2xl">{product.name}</h1>
+        <p>{product.description}</p>
+        <div className="flex items-center gap-4 mt-4">
+          <span className="text-xl">₦{product.price}</span>
+          <button
+            onClick={() => {
+              addItemToCart(product._id);
+            }}
+            className="primary-btn px-4 py-1 flex items-center gap-2"
+          >
+            <CartLogo />
+            Add to cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
