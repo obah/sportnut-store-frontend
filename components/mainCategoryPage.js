118308139f016f0440b7d298a0cb0cea13503cfb
Obaloluwa Olusoji <olusojiobah@gmail.com>
2023-07-11 01:10:02 +0100

    added brands page

--- components/mainCategoryPage.js
+++ components/mainCategoryPage.js
@@ -43,7 +43,6 @@ export default function MainCategoryPage({ mainCategory, subCategory }) {
   let imageAlt = "";
   let mainText = "";
   let content = "";
-  let link = "";
   let tagline = "";
 
   switch (mainCategory.categoryName) {
@@ -105,74 +104,178 @@ export default function MainCategoryPage({ mainCategory, subCategory }) {
       tagline = "Accessories";
   }
 
+  const letters = [
+    "#",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+
+  const brandStyle = "underline font-semibold hover:cursor-pointer";
+
   return (
     <div className="big-center">
-      <div className="flex border-b border-b-neutral-400 mb-14">
-        <div className="w-1/4">
-          <h2 className="py-8 px-4 font-semibold text-2xl border-b">
-            {tagline}
-          </h2>
-          {subCategory.map((category) => (
-            <div key={category._id} className="px-2">
-              <Link
-                href={"/sp/" + category._id}
-                className="flex items-center gap-4 h-20 px-2 rounded-sm border-b border-b-neutral-400 border-l-4 border-l-transparent hover:border-l-secondary hover:border-r hover:border-t hover:font-semibold hover:shadow-md"
+      {mainCategory._id === "64a32f83981ca04e0c3099e4" ? (
+        <div className="px-72">
+          {/* <div></div> */}
+          <h1 className="py-10 font-bold text-4xl border-b text-center">
+            SHOP ALL BRANDS
+          </h1>
+          <h3 className="py-4 font-semibold text-2xl text-center">Est. 2022</h3>
+          <p className="mt-3 mb-16">
+            SportNut Store is proud to offer high-quality sporting goods
+            equipment and gear from the top brands you trust. Since 2022,
+            SportNut Store has brought you the brands that make you untouchable
+            on the field, on the court and anywhere else you play your sport.
+            Now you can easily find the brands from our stores on our Web site.
+            From our collection of apparel and footwear from{" "}
+            <span className={brandStyle}>The North Face</span>,{" "}
+            <span className={brandStyle}>Nike</span> and{" "}
+            <span className={brandStyle}>Under Armour</span>, to our premium
+            equipment from <span className={brandStyle}>TaylorMade</span>,{" "}
+            <span className={brandStyle}>Field & Stream</span> and{" "}
+            <span className={brandStyle}>DeMarini</span>, you’ll find all the
+            best brands and gear right here.
+          </p>
+          <div className="flex justify-center text-md text-primary mb-4">
+            {letters.map((letter) => (
+              <p
+                key={letter}
+                className="w-6 text-center border-r last:border-r-0 border-r-black hover:cursor-pointer"
               >
-                <img src={category.image} alt="" className="h-14" />
-                {category.categoryName}
-              </Link>
+                {letter}
+              </p>
+            ))}
+          </div>
+          <div className="border border-neutral-500">
+            <div className="border border-neutral-500">
+              <h3 className="border border-neutral-500 py-2 px-4 font-semibold text-lg">
+                A
+              </h3>
+              <div>
+                <ul className="border marker:text-black list-disc list-inside">
+                  <li className="py-4 px-4 text-md">
+                    <Link href={"/sp/64a331f6a7a4adbcb9ca3d3a"}>Adidas</Link>
+                  </li>
+                </ul>
+              </div>
             </div>
-          ))}
-        </div>
-        <div className="w-3/4 relative">
-          <img
-            src={mainImage}
-            alt={imageAlt}
-            className="object-cover rounded-md"
-          />
-          <div className="flex flex-col gap-3 justify-center text-white w-1/2 h-full absolute top-0 left-14">
-            <h1 className="font-black text-6xl [text-shadow:0_4px_8px_var(--tw-shadow-color)] shadow-black">
-              {mainText}
-            </h1>
-            <p className="font-bold text-lg [text-shadow:0_4px_8px_var(--tw-shadow-color)] shadow-black">
-              {content}
-            </p>
-            <Link
-              href={"/pp/" + mainCategory._id}
-              className="primary-btn w-1/3 text-center py-3"
-            >
-              SHOP NOW
-            </Link>
+            <div className="border border-neutral-500">
+              <h3 className="border border-neutral-500 py-2 px-4 font-semibold text-lg">
+                N
+              </h3>
+              <div>
+                <ul className="border marker:text-black list-disc list-inside">
+                  <li className="py-4 px-4 text-md">
+                    <Link href={"/sp/64a331eba7a4adbcb9ca3d36"}>Nike</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="my-10 text-center">
+            <h3 className="font-extrabold text-3xl text-primary">
+              More favorite brands coming soon...
+            </h3>
           </div>
         </div>
-      </div>
-      <div>
-        <MiniBlog
-          category={mainCategory.categoryName}
-          categoryId={mainCategory._id}
-        />
-      </div>
-      <div className="mt-20">
-        <h2 className="font-black text-5xl text-center mb-6">
-          <span className="text-primary">{`SPORTNUT'S `}</span>PRO TIPS
-        </h2>
-        <div className="flex gap-6 justify-center px-6">
-          {tipsData.map((tip) => (
-            <div key={tip.id} className="w-1/4 text-center border">
-              <Link href={"/ip"}>
-                {" "}
-                {/**link this and all others to a page that says coming soon */}
-                <Image src={tip.image} alt={tip.imageAlt} className="mb-3" />
-                <h3 className="font-bold p-3 pb-1">{tip.title}</h3>
-                <div className="w-10 mx-auto pb-3 border-t border-t-secondary"></div>
-                <p className="text-sm px-3 pt-0 pb-9">
-                  <span className="line-clamp-3 ">{tip.text}</span>
-                </p>
-              </Link>
+      ) : (
+        <>
+          <div className="flex border-b border-b-neutral-400 mb-14">
+            <div className="w-1/4">
+              <h2 className="py-8 px-4 font-semibold text-2xl border-b">
+                {tagline}
+              </h2>
+              {subCategory.map((category) => (
+                <div key={category._id} className="px-2">
+                  <Link
+                    href={"/sp/" + category._id}
+                    className="flex items-center gap-4 h-20 px-2 rounded-sm border-b border-b-neutral-400 border-l-4 border-l-transparent hover:border-l-secondary hover:border-r hover:border-t hover:font-semibold hover:shadow-md"
+                  >
+                    <img src={category.image} alt="" className="h-14" />
+                    {category.categoryName}
+                  </Link>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-      </div>
+            <div className="w-3/4 relative">
+              <img
+                src={mainImage}
+                alt={imageAlt}
+                className="object-cover rounded-md"
+              />
+              <div className="flex flex-col gap-3 justify-center text-white w-1/2 h-full absolute top-0 left-14">
+                <h1 className="font-black text-6xl [text-shadow:0_4px_8px_var(--tw-shadow-color)] shadow-black">
+                  {mainText}
+                </h1>
+                <p className="font-bold text-lg [text-shadow:0_4px_8px_var(--tw-shadow-color)] shadow-black">
+                  {content}
+                </p>
+                <Link
+                  href={"/pp/" + mainCategory._id}
+                  className="primary-btn w-1/3 text-center py-3"
+                >
+                  SHOP NOW
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div>
+            <MiniBlog
+              category={mainCategory.categoryName}
+              categoryId={mainCategory._id}
+            />
+          </div>
+          <div className="mt-20">
+            <h2 className="font-black text-5xl text-center mb-6">
+              <span className="text-primary">{`SPORTNUT'S `}</span>PRO TIPS
+            </h2>
+            <div className="flex gap-6 justify-center px-6">
+              {tipsData.map((tip) => (
+                <div key={tip.id} className="w-1/4 text-center border">
+                  <Link href={"/ip"}>
+                    {" "}
+                    {/**link this and all others to a page that says coming soon */}
+                    <Image
+                      src={tip.image}
+                      alt={tip.imageAlt}
+                      className="mb-3"
+                    />
+                    <h3 className="font-bold p-3 pb-1">{tip.title}</h3>
+                    <div className="w-10 mx-auto pb-3 border-t border-t-secondary"></div>
+                    <p className="text-sm px-3 pt-0 pb-9">
+                      <span className="line-clamp-3 ">{tip.text}</span>
+                    </p>
+                  </Link>
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
