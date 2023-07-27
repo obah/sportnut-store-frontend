eefb19e65d526934b1d1c7bb642cc915a4fd1e51
Obaloluwa Olusoji <olusojiobah@gmail.com>
2023-07-27 16:37:46 +0100

    added more viewports to center width

--- components/viewPorts.js
+++ components/viewPorts.js
@@ -1,6 +1,11 @@
 export function Center({ children, styles }) {
   return (
-    <div className={"max-w-screenn-2xl mx-auto my-0 px-6 py-0 " + styles}>
+    <div
+      className={
+        "mx-auto my-0 max-w-screen-md px-6 py-0 md:max-w-screen-lg lg:max-w-screen-2xl " +
+        styles
+      }
+    >
       {children}
     </div>
   );
@@ -8,6 +13,13 @@ export function Center({ children, styles }) {
 
 export function BigCenter({ children, styles }) {
   return (
-    <div className={"mx-auto my-0 max-w-screen-2xl " + styles}>{children}</div>
+    <div
+      className={
+        "mx-auto my-0 max-w-screen-md md:max-w-screen-lg lg:max-w-screen-2xl " +
+        styles
+      }
+    >
+      {children}
+    </div>
   );
 }
