fe8e41caaa576f7ec534a7ca6bc0ca108a6a5bb9
Obaloluwa Olusoji <olusojiobah@gmail.com>
2023-07-30 18:53:20 +0100

    bug fixes

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -119,7 +119,7 @@ export default function Page() {
         <Center
           styles={"flex h-screen items-center justify-center bg-neutral-100"}
         >
-          <div className="flex h-1/2  w-full flex-col items-center justify-center gap-5 rounded-sm bg-white  p-8 text-center shadow shadow-black md:w-1/2 lg:w-1/3">
+          <div className="flex h-1/2 w-full flex-col items-center justify-center gap-5 rounded-sm bg-white  p-8 text-center shadow shadow-black md:w-1/2 lg:w-1/3">
             <h1 className="text-3xl font-bold">Thanks for your order</h1>
             <p className="text-lg font-semibold">
               Please check your mail for other details concerning your order.
