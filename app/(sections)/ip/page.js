264549322ee5b9108082d566d971919dad17035f
Obaloluwa Olusoji <olusojiobah@gmail.com>
2023-11-14 12:57:38 +0100

    chore: changed content for coming soon pages

--- app/(sections)/ip/page.js
+++ app/(sections)/ip/page.js
@@ -4,10 +4,10 @@ export default function Page() {
   return (
     <BigCenter styles={"bg-primary text-center text-white"}>
       <h1 className="pb-3 pt-20 text-4xl font-semibold lg:pt-40 lg:text-7xl">
-        This content is still a work in progress
+        We are updating some info...
       </h1>
       <p className=" pb-20 text-xl font-semibold lg:pb-40 lg:text-3xl">
-        It will be available very soon, so please check back later
+        Please check back later
       </p>
     </BigCenter>
   );
